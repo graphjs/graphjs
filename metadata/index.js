89c17353d465b65ad8ec2a025976270182ce990e
codekaar <kharelbarun@gmail.com>
2019-04-18 09:42:51 +0545

    fix

--- metadata/index.js
+++ metadata/index.js
@@ -37,8 +37,10 @@ module.exports = {
             var value = meta.value.replace(/\\/g, '\\\\').replace(/\{/g, '\\{').replace(/\}/g, '\\}');
             return `[metadata](type=${type};key={${key}};value={${value}})`;
         }).join('\n');
+        content = content ? content : '';
+        metastr = metastr ? '\n' + metastr : '';
 
-        return content + '\n' + metastr;
+        return content + metastr;
     },
 };
 
