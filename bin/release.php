1de182feb99b89e231ad159448a3289fbebded00
Emre <esokullu@gmail.com>
2019-09-29 18:20:02 -0700

    2.4

--- bin/release.php
+++ bin/release.php
@@ -21,10 +21,10 @@ echo "Starting\n";
 
 file_put_contents($me."/VERSION", $version);
 
-$groupsv2_op = "cd {$me} && NEWVERSION={$version} && modules=groupsv2 npm run build && cp {$dist_dir}/graph* {$groups_frontend_path}/site/vendor/graphjs/ && cd {$groups_frontend_path} && git commit -am \$NEWVERSION && git tag graphjs-\$NEWVERSION && git push && git push --tag";
+$groupsv2_op = "cd {$groups_frontend_path} && git pull && cd {$me} && NEWVERSION={$version} && modules=groupsv2 npm run build && cp {$dist_dir}/graph* {$groups_frontend_path}/site/vendor/graphjs/ && cd {$groups_frontend_path} && git commit -am \$NEWVERSION && git tag graphjs-\$NEWVERSION && git push && git push --tag";
 exec($groupsv2_op);
 
-$graphjs_website_op = "cd {$me} && NEWVERSION={$version} && modules=all npm run build && mkdir {$graphjs_website_path}/app/dist/\$NEWVERSION && cp {$dist_dir}/graph* {$graphjs_website_path}/app/dist/\$NEWVERSION && cd {$graphjs_website_path} && git add app/dist/\$NEWVERSION && git commit -am \$NEWVERSION && git tag graphjs-\$NEWVERSION && git push && git push --tags";
+$graphjs_website_op = "cd {$graphjs_website_path} && git pull && cd {$me} && NEWVERSION={$version} && modules=all npm run build && mkdir {$graphjs_website_path}/app/dist/\$NEWVERSION && cp {$dist_dir}/graph* {$graphjs_website_path}/app/dist/\$NEWVERSION && cd {$graphjs_website_path} && git add app/dist/\$NEWVERSION && git commit -am \$NEWVERSION && git tag graphjs-\$NEWVERSION && git push && git push --tags";
 exec($graphjs_website_op);
 
 exec("cd {$me} && git commit -am {$version} && git tag {$version} && git push && git push --tags");
