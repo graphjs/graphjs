9ffca93d07e9167e03d906677a3859db4dc51417
Emre Sokullu <esokullu@gmail.com>
2020-02-18 11:05:25 +0300

    script update

--- bin/release.php
+++ bin/release.php
@@ -20,13 +20,20 @@ $me = dirname(__DIR__);
 echo "Starting\n";
 
 file_put_contents($me."/VERSION", $version);
+echo "Version file written\n";
 
+echo "Starting Groups Frontend Ops\n";
 $groupsv2_op = "cd {$groups_frontend_path} && git pull && cd {$me} && NEWVERSION={$version} && modules=groupsv2 npm run build && cp {$dist_dir}/graph* {$groups_frontend_path}/site/vendor/graphjs/ && cd {$groups_frontend_path} && git commit -am \$NEWVERSION && git tag graphjs-\$NEWVERSION && git push && git push --tag";
 exec($groupsv2_op);
+echo "Ended Groups Frontend Ops\n";
 
+echo "Starting GraphJS Website Ops\n";
 $graphjs_website_op = "cd {$graphjs_website_path} && git pull && cd {$me} && NEWVERSION={$version} && modules=all npm run build && mkdir {$graphjs_website_path}/app/dist/\$NEWVERSION && cp {$dist_dir}/graph* {$graphjs_website_path}/app/dist/\$NEWVERSION && cd {$graphjs_website_path} && git add app/dist/\$NEWVERSION && git commit -am \$NEWVERSION && git tag graphjs-\$NEWVERSION && git push && git push --tags";
 exec($graphjs_website_op);
+echo "Ended GraphJS Website Ops\n";
 
+echo "Starting GraphJS Ops\n";
 exec("cd {$me} && git commit -am {$version} && git tag {$version} && git push && git push --tags");
+echo "Ended GraphJS Ops\n";
 
 echo "Complete...\n";
